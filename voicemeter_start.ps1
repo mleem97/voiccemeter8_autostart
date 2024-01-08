ae0309b717a40f75ee5a68abbb4e2eac6b372709
Marvin <52848568+mleem97@users.noreply.github.com>
2024-01-08 03:09:18 +0100

    Update voicemeter_start.ps1

--- voicemeter_start.ps1
+++ voicemeter_start.ps1
@@ -24,9 +24,12 @@ $audioProcess = Get-Process -Name Audiodg
 if ($audioProcess) {
     $audioProcess.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
     $audioProcess.ProcessorAffinity = 1
-    Write-Host "Einstellungen für Audiodg.exe erfolgreich angewendet."
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show("Audiodg wurde angepasst.", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 } else {
-    Write-Host "Der Prozess 'Audiodg.exe' wurde nicht gefunden."
+# Der Rückgabewert ist 1, führe andere Aktionen aus
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show("Audiodg wurde nicht angepasst.", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
 # Überprüfe, ob "voicemeeter8x64.exe" läuft
@@ -37,12 +40,19 @@ if ($voicemeeterProcess) {
     # Setze die Priorität und Affinität für "voicemeeter8x64.exe"
     $voicemeeterProcess.PriorityClass = [System.Diagnostics.ProcessPriorityClass]::High
     $voicemeeterProcess.ProcessorAffinity = 1
-    Write-Host "Einstellungen für $voicemeeterProcessName erfolgreich angewendet."
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show("Voicemeter wurde angepasst.", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 } else {
     # Starte "voicemeeter8x64.exe", falls nicht gestartet
     Start-Process -FilePath "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8x64.exe"
-    Write-Host "$voicemeeterProcessName wurde gestartet."
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.MessageBox]::Show("Voicemeter wurde gestartet.", "Info", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 }
 
+# Nach erfolgreicher Ausführung:
+# Zeige ein Erfolgsmeldungsfenster an
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show(":) Alles Gucci", "Erfolg", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 # Rückgabewert 0 für Erfolg
 return 0
+
